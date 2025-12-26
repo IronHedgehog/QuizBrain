5dc61b126a84d130ff758b6375462669905d3e77
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-12-26 18:45:56 +0200

    remove comments

--- tests/test_quiz_brain.py
+++ tests/test_quiz_brain.py
@@ -1,6 +1,7 @@
 from quiz_brain import QuizBrain
 from question_model import Question
 
+
 def test_check_answer_correct():
     q = Question("Test question?", "True")
     quiz = QuizBrain([q])
@@ -8,6 +9,7 @@ def test_check_answer_correct():
     result = quiz.check_answer("true", "True")
     assert result is True
     assert quiz.score == 1
+
 
 def test_check_answer_incorrect():
     q = Question("Test question?", "True")
